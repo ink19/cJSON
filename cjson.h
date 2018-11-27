e433b9ac430d86e274cdae2b46eab50cd23cebfb
ink19 <nkzxwgr@163.com>
2018-11-27 22:45:45 +0800

    添加基本类型

--- cjson.h
+++ cjson.h
@@ -1,5 +1,31 @@
 #ifndef __INK19_CJSON_H__
 #define __INK19_CJSON_H__
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+typedef struct {
+
+} cjson_number_t;
+
+typedef struct {
+
+} cjson_string_t;
+
+typedef struct {
+
+} cjson_boolean_t;
+
+typedef struct {
+
+} cjson_set_t;
+
+typedef struct {
+
+} cjson_object_t;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
